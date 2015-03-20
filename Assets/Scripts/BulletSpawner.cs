9b7da8c127f2cb3291d4dc9e2c6e3ed1b6748c22
Inge Becht <inge.becht91@gmail.com>
2015-03-20 21:18:20 +0100

    working on projectile avoidance game

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpawner : MonoBehaviour {
+
+	public GameObject objectToSpawn;
+	public GameObject targetObject;
+
+	public float maxDistance;
+	public float minDistance;
+
+	public float maxSpeed;
+	public float minSpeed;
+
+	public float minIntervalBetweenProjectiles;
+	public float maxIntervalBetweenProjectiles;
+
+	public bool projectileIs3D;
+
+	private float timeSinceLastProjectile;
+	private Vector3 targetPosition;
+
+	void Start () {
+		timeSinceLastProjectile = Time.time;
+		targetPosition = targetObject.transform.position;
+	}
+	
+
+	void Update () {
+		if(Time.time - timeSinceLastProjectile > maxIntervalBetweenProjectiles)
+		{
+
+			timeSinceLastProjectile = Time.time;
+			GameObject projectile = Instantiate(objectToSpawn);
+
+			float radius = Random.Range(minDistance, maxDistance);
+
+			// start position should be on a circle around the user.  
+			float radians = Random.Range (0, Mathf.PI * 2);
+			float x = targetPosition.x + radius * Mathf.Cos(radians);
+			float y = 0;
+			float z = targetPosition.y + radius * Mathf.Sin(radians);
+
+			projectile.transform.position = new Vector3(x, y, z);
+
+		}
+	}
+}
---
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+	private Vector3 headingDirection;
+	private float speed;
+	private GameObject target;
+
+	private Vector3 startingPosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollsionEnter(Collision c)
+	{
+		if (c.gameObject.GetInstanceID () == target.GetInstanceID ()) {
+			Destroy (this);
+		}
+	}
+
+
+}
