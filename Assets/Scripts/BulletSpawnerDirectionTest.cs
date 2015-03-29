183e1f6ae965d81bf4104d2894602640f6e62e3c
Tim <t.d.haberham@students.uu.nl>
2015-03-29 20:54:14 +0200

    Refactoring direction code

--- Assets/Scripts/BulletSpawnerDirectionTest.cs
+++ Assets/Scripts/BulletSpawnerDirectionTest.cs
@@ -39,24 +39,36 @@ public class BulletSpawnerDirectionTest : MonoBehaviour
     {
         playerPosition = playerObject.transform.position;
         ballsGuessed = new List<GameObject>();
-        if (writeOutput & !isExample)
+        balls = new List<GameObject>();
+
+        if (writeOutput)
         {
             csvWriter = new CsvWriter("Audio"+ amountOfSounds +"DirectionEstimateTest.txt", "Object number, Actual position, Estimated postion, Error angle");
             csvWriter.writeLineToFile("-- " + "amount:" + amountOfSounds +" ,3D:" + directionIs3D + " ,only front:" + directionOnlyFront +" ,unique sounds:"+uniqueSounds);
         }
 
-        balls = new List<GameObject>();
-
         StartCoroutine(spawnBallsAfterInterval());
     }
-
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1") && !spawning)
         {
-            makeDirectionEstimate();
-        }       
+            Vector3 lookDirection = getLookDirection();
+            markEstimatedBall(lookDirection);
+        }
+        // all guesses have been made
+        if (ballsGuessed.Count == balls.Count && balls.Count > 0)
+        {
+            if (writeOutput)
+                csvWriter.writeLineToFile("------------Done------------------");
+            Debug.Log("completed with guessing positions");
+
+            DestroyBalls();
+
+            // go back to gui
+            gui.SetActive(true);
+        }
     }
 
     // spawn a given number of balls
@@ -64,7 +76,7 @@ public class BulletSpawnerDirectionTest : MonoBehaviour
     {
         spawning = true;
         int amount = amountOfSounds;
-        if (uniqueSounds)
+        if (uniqueSounds) // if unique sounds are required, you can't spawn more than there are sounds
             amount = audioClips.Count < amount ? audioClips.Count : amount;
 
         yield return new WaitForSeconds(startDelay);
@@ -73,26 +85,58 @@ public class BulletSpawnerDirectionTest : MonoBehaviour
             if (i > 0 && audioSpawnDelay > 0)
             {
                 yield return new WaitForSeconds(audioSpawnDelay);
-                spawnBall(i);
+                spawnBall(i, randomPosition());
             }
             else
-                spawnBall(i);
+                spawnBall(i, randomPosition());
         }
         spawning = false;
         yield return null;
     }
 
+    // spawn a static ball that emits sound
+    private void spawnBall(int id, Vector3 ballPosition)
+    {
+        GameObject ball = Instantiate(objectToSpawn);
 
-    private void makeDirectionEstimate()
+        // use a static projectile for now
+        ball.GetComponent<ProjectileBehaviour>().Init(ballPosition, ballPosition, 0, ball, csvWriter);
+        ball.GetComponent<ProjectileBehaviour>().SetVisible(true);
+
+        ball.AddComponent<AudioSource>();
+        if (uniqueSounds)
+            ball.GetComponent<AudioSource>().clip = audioClips[id];
+        else
+            ball.GetComponent<AudioSource>().clip = audioClips[0];
+
+        ball.GetComponent<AudioSource>().playOnAwake = true;
+        ball.GetComponent<AudioSource>().loop = true;
+        ball.AddComponent<OSPAudioSource>();
+
+        balls.Add(ball);
+        if (!directionIs3D)
+            Debug.Log("2D Ball Spawned at position " + ballPosition.ToString());
+        else
+            Debug.Log("3D Ball Spawned at position " + ballPosition.ToString());
+    }
+
+
+    //get the direction the oculus is facing
+    private Vector3 getLookDirection()
+    {
+        //get oculus look direction
+        var centerEyeAnchor = playerObject.transform.GetChild(1);
+        Vector3 lookForward = centerEyeAnchor.forward;
+        if (!directionIs3D)
+            lookForward.y = 0;
+        return lookForward;
+    }
+
+    //mark the ball closest to the current looking direction, to determine the most likely error
+    private void markEstimatedBall(Vector3 lookDirection)
     {
         if (ballsGuessed.Count < balls.Count) // check if there are any balls left
         {
-            //get oculus look direction
-            var centerEyeAnchor = playerObject.transform.GetChild(1);
-            Vector3 lookForward = centerEyeAnchor.forward;
-            if (!directionIs3D)
-                lookForward.y = 0;
-
             // loop to find the ball closest to the looking direction
             GameObject closestBall = null;
             float closestAngle = float.MaxValue;
@@ -101,7 +145,7 @@ public class BulletSpawnerDirectionTest : MonoBehaviour
                 GameObject currentBall = balls[i];
                 if (!ballsGuessed.Contains(currentBall))
                 {
-                    float angle = Vector3.Angle(lookForward, currentBall.transform.position);
+                    float angle = Vector3.Angle(lookDirection, currentBall.transform.position);
                     if (angle < closestAngle)
                     {
                         closestBall = currentBall;
@@ -114,23 +158,12 @@ public class BulletSpawnerDirectionTest : MonoBehaviour
             {
                 ballsGuessed.Add(closestBall);
                 //write error
-                if (writeOutput & !isExample)
+                if (writeOutput)
                 {
-                    Vector3 estimatedPosition = lookForward * radius;
+                    Vector3 estimatedPosition = lookDirection * radius;
                     csvWriter.writeLineToFile(ballsGuessed.Count + ", " + closestBall.transform.position.ToString() + ", " + estimatedPosition.ToString() + ", " + closestAngle);
                 }
                 Debug.Log("Guess processed " + (balls.Count - ballsGuessed.Count) + " more to go");
-            }
-
-            if (ballsGuessed.Count == balls.Count && balls.Count > 0)
-            {
-                if (writeOutput)
-                    csvWriter.writeLineToFile("------------Done------------------");
-                Debug.Log("completed with guessing positions");
-                
-                DestroyBalls();
-                // go back to gui
-                gui.SetActive(true);
             }
         }
     }
@@ -148,15 +181,13 @@ public class BulletSpawnerDirectionTest : MonoBehaviour
         balls.Clear();
     }
 
-    // spawn a static ball that emits sound
-    private void spawnBall(int id)
+    
+    //generate a random position around the player on either a 2d circle or 3d sphere
+    private Vector3 randomPosition()
     {
-        GameObject ball = Instantiate(objectToSpawn);
-
         // start position should be on a circle/sphere around the user.  
-        float x, z, y, radiansX= 0, radiansY = 0;
-        //radX 0 -> right, radx pi -> left
-        //radY 0 -> top, radY pi -> bottom
+        float radiansX;
+        Vector3 position;
 
         if (directionOnlyFront)
             radiansX = Random.Range(0, Mathf.PI);
@@ -164,42 +195,37 @@ public class BulletSpawnerDirectionTest : MonoBehaviour
             radiansX = Random.Range(0, Mathf.PI * 2);
         if (directionIs3D)
         {
-            // radiansY has heightLimit to prevent placement exactly below/above the player
-            radiansY =  Random.Range(heightLimit * Mathf.PI, (1.0f - heightLimit) * Mathf.PI); 
-            x = playerPosition.x + radius * Mathf.Cos(radiansX) * Mathf.Sin(radiansY);
-            y = playerPosition.z + radius * Mathf.Cos(radiansY);
-            z = playerPosition.y + radius * Mathf.Sin(radiansX) * Mathf.Sin(radiansY);        
+            float radiansY = Random.Range(heightLimit * Mathf.PI, (1.0f - heightLimit) * Mathf.PI);
+            position = positionOnSphere(radiansX, radiansY);
         }
         else // 2d circle
-        {
-            x = playerPosition.x + radius * Mathf.Cos(radiansX);
-            y = 0;
-            z = playerPosition.y + radius * Mathf.Sin(radiansX);
-        }
+            position = positionOnCircle(radiansX);
 
-        Vector3 ballPosition = new Vector3(x, y, z);
-        
-        // use a static projectile for now
-        ball.GetComponent<ProjectileBehaviour>().Init(ballPosition, ballPosition, 0, ball, csvWriter);
-        ball.GetComponent<ProjectileBehaviour>().SetVisible(true);
-    
-        ball.AddComponent<AudioSource>();
-        if (uniqueSounds)
-            ball.GetComponent<AudioSource>().clip = audioClips[id];
-        else
-            ball.GetComponent<AudioSource>().clip = audioClips[0];
- 
-        ball.GetComponent<AudioSource>().playOnAwake = true;
-        ball.GetComponent<AudioSource>().loop = true;
-        ball.AddComponent<OSPAudioSource>();
-        
-        balls.Add(ball);
-        if (!directionIs3D)
-            Debug.Log("2D Ball Spawned at " + radiansX / Mathf.PI + "pi radiansX with position " + ballPosition.ToString());
-        else
-            Debug.Log("3D Ball Spawned at " + radiansX / Mathf.PI + "pi radiansX, "+ radiansY/Mathf.PI +"pi radiansY with position " + ballPosition.ToString());
-
+        return position;
     }
 
+    // generate a position on a sphere
+    private Vector3 positionOnSphere(float radiansX, float radiansY)
+    {
+        //radX 0 -> right, radx pi -> left
+        //radY 0 -> top, radY pi -> bottom
+        float x, z, y;
+        // radiansY has heightLimit to prevent placement exactly below/above the player
+        x = playerPosition.x + radius * Mathf.Cos(radiansX) * Mathf.Sin(radiansY);
+        y = playerPosition.z + radius * Mathf.Cos(radiansY);
+        z = playerPosition.y + radius * Mathf.Sin(radiansX) * Mathf.Sin(radiansY);
+        return new Vector3(x, y, z);
+    }
+
+    // generate a position on a circle at y=0
+    private Vector3 positionOnCircle(float radians)
+    {
+        //rad 0 -> right, rad pi -> left
+        float x, z, y;
+        x = playerPosition.x + radius * Mathf.Cos(radians);
+        y = 0;
+        z = playerPosition.y + radius * Mathf.Sin(radians);
+        return new Vector3(x, y, z);
+    }
 
 }
