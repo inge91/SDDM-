b416ec3b01146c67a73847726ef423d7f0267166
inge91 <inge.becht91@gmail.com>
2015-03-30 20:59:13 +0200

    rework of csvwriter. it now adds a number in case filename already exists

--- Assets/Scripts/CsvWriter.cs
+++ Assets/Scripts/CsvWriter.cs
@@ -1,14 +1,26 @@
 using System;
+using System.IO;
 namespace AssemblyCSharp
 {
 	public class CsvWriter
 	{
 
-		private string direction = "TestResults/";
+		private string directory = "TestResults/";
 		System.IO.StreamWriter file;
 		public CsvWriter (string filename, string header)
 		{
-			file = new System.IO.StreamWriter (direction + filename);
+			// Find filename in directory and add number to distinguish between already existing files.
+			DirectoryInfo logDirectory = new DirectoryInfo(directory);
+			int i = 1;
+			while (true) {
+				FileInfo[] taskFiles = logDirectory.GetFiles (filename + i.ToString() + ".csv");
+				if(taskFiles.Length == 0)
+				{
+					break;
+				}
+				i++;
+			}
+			file = new System.IO.StreamWriter (directory + filename + i.ToString() + ".csv");
 			file.WriteLine (header);
 		}
 
@@ -21,6 +33,12 @@ namespace AssemblyCSharp
 		{
 			file.WriteLine (line);
 		}
+
+		public void Close()
+		{
+			file.Close();
+		}
+	
 	}
 }
 
