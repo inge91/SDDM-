30dc4668d54f9cdf828b128a819615d5f23842ce
MiloBuwalda <milo.buwalda@gmail.com>
2015-04-02 18:07:44 +0200

    shoot script

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    public int speed = 2;
+
+    public float timing;
+
+
+	// Use this for initialization
+	void Start () {
+        speed = 2;
+        timing = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // On left mouse click and on space shoot
+    void ontriggerEnter() {
+
+    }
+}
