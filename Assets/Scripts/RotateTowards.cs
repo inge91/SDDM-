c7acd43b9b89733d39c47ad217e3a80aa8c6ece2
JohnHeukers <John_heukers@hotmail.com>
2015-04-03 18:52:33 +0200

    New game scene setup

---
+++ Assets/Scripts/RotateTowards.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Rotate the object to which the script is attachted towards a given target object
+//Adjust the speed parameter to adjust the rotation speed
+public class RotateTowards : MonoBehaviour {
+	public GameObject target; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int speed = 1000;
+		var q = Quaternion.LookRotation(target.transform.position - transform.position);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, q, speed * Time.deltaTime);
+	}
+}
