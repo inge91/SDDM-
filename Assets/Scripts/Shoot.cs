2d703414efaacce4928f9d196f70581d8f337760
MiloBuwalda <milo.buwalda@gmail.com>
2015-04-08 22:26:11 +0200

    Audio charge

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -7,6 +7,10 @@ public class Shoot : MonoBehaviour {
 	public bool autoFire = false;
 	public bool rumbleOn = false;
 
+    public bool useChargingSound;
+
+    public AudioClip chargingSound;
+
     public float fireRate;
     public Transform shotSpawn;
     public GameObject shot;
@@ -14,12 +18,23 @@ public class Shoot : MonoBehaviour {
     private ControllerRumble rumble;
 
     private float nextFire;
+    private float chargeTime;
+    private float nextCharge;
 
 	// Use this for initialization
 	void Start () {
         //speed = 2;
         //fireRate = 1;
-		nextFire = Time.time + fireRate;
+
+        nextFire = Time.time + fireRate;
+        if (chargingSound != null)
+        {
+            chargeTime = chargingSound.length;
+            nextFire = nextFire + chargeTime;
+        }
+
+        nextCharge = Time.time + fireRate;
+
 
         GameObject temp = GameObject.Find("RumbleObject");
         rumble = (ControllerRumble)temp.GetComponent(typeof(ControllerRumble));
@@ -27,8 +42,13 @@ public class Shoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		if(autoFire){
+            if (useChargingSound && Time.time > nextCharge)
+            {
+                
+                AudioSource.PlayClipAtPoint(chargingSound, this.transform.position);
+                nextCharge = Time.time + fireRate;
+            }
 			if (Time.time > nextFire) {
 				nextFire = Time.time + fireRate;
 				Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
